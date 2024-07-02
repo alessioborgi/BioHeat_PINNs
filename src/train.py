ab4352a227682ba9bc0452e9162daa0ba65cb9bd
Alessio_Borgi <borgi.1952442@studenti.uniroma1.it>
2024-07-02 07:37:30 +0200

    Add ablation studies details

--- src/train.py
+++ src/train.py
@@ -170,10 +170,10 @@ def compute_metrics(true, pred):
     small_number = 1e-40
     true_nonzero = np.where(true != 0, true, small_number)
     
-    MSE = dde.metrics.mean_squared_error(true, pred)
-    MAE = np.mean(np.abs((true - pred) / true_nonzero)) 
-    L2RE = dde.metrics.l2_relative_error(true, pred)
-    max_APE = np.max(np.abs((true - pred) / true_nonzero)) 
+    MSE = dde.metrics.mean_squared_error(true, pred)        # Mean Squared Error
+    MAE = np.mean(np.abs((true - pred) / true_nonzero))     # Mean Absolute Error
+    L2RE = dde.metrics.l2_relative_error(true, pred)        # L2 Relative Error
+    max_APE = np.max(np.abs((true - pred) / true_nonzero))  # 
     
     metrics = {
         "MSE": MSE,
