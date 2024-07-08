3e71087415ae6e50eb9a46d5de60c3d582a2b1c8
Alessio_Borgi <borgi.1952442@studenti.uniroma1.it>
2024-07-08 16:37:16 +0200

    Modified PDE 2D

--- src/pde.py
+++ src/pde.py
@@ -9,6 +9,7 @@ from configurations import HydraConfigStore
 # import tensorflow as tf
 
 
+
 f1, f2, f3 = [None]*3
 
 def mish(x):
@@ -20,7 +21,7 @@ def softplus(x):
 def aptx(x):
     return (1 + torch.tanh(x)) * (x / 2)
 
-def boundary_0(x, on_boundary):
+def boundary_x0(x, on_boundary):
     """
     Boundary condition function for x = 0.
     
@@ -33,7 +34,7 @@ def boundary_0(x, on_boundary):
     """
     return on_boundary and np.isclose(x[0], 0)
 
-def boundary_1(x, on_boundary):
+def boundary_x1(x, on_boundary):
     """
     Boundary condition function for x = 1.
     
@@ -46,75 +47,46 @@ def boundary_1(x, on_boundary):
     """
     return on_boundary and np.isclose(x[0], 1)
 
-
-
-
-# Define constants
-a1 = 1.0  # example value
-a2 = 1.0  # example value
-a3 = 1.0  # example value
-omega = 1.0  # example value
-Q = lambda x: np.sin(np.pi * x)  # example source term
-
-# Define the geometry and time domain
-geom = dde.geometry.Interval(0, 1)
-timedomain = dde.geometry.TimeDomain(0, 1)
-geomtime = dde.geometry.GeometryXTime(geom, timedomain)
-
-# Define the PDE
-def pde(x, theta):
-    dtheta_dt = dde.grad.jacobian(theta, x, i=0, j=1)
-    dtheta_dx2 = dde.grad.hessian(theta, x, component=0, i=0, j=0)
-    return a1 * dtheta_dt - dtheta_dx2 + a2 * omega * theta - a3 * Q(x[:, 0:1])
-
-# Define boundary conditions
-def boundary_left(x, on_boundary):
-    return on_boundary and np.isclose(x[0], 0)
-
-def boundary_right(x, on_boundary):
-    return on_boundary and np.isclose(x[0], 1)
-
-c0 = 1.0  # example value for theta(0, tau)
-v_tau = lambda x: np.cos(np.pi * x[:, 1])  # example function for v(tau)
-
-# Define initial condition
-theta_0 = lambda x: np.sin(np.pi * x[:, 0])  # example initial condition
-ic = dde.icbc.IC(geomtime, theta_0, lambda _, on_initial: on_initial)
-
-# Define the data object
-data = dde.data.TimePDE(geomtime, pde, [bc_left, bc_right, ic], num_domain=40, num_boundary=10, num_initial=10)
-
-# Define the neural network
-net = dde.maps.FNN([2] + [20] * 3 + [1], "tanh", "Glorot uniform")
-
-# Define the model
-model = dde.Model(data, net)
-
-# Compile and train the model
-model.compile("adam", lr=1e-3)
-model.train(epochs=10000)
-
-# Predict and plot the solution
-X = geomtime.random_points(100)
-y_pred = model.predict(X)
-dde.saveplot(y_pred, X, "pde_solution")
-
-
-
-
-def bc0_obs(x, theta, X):
+def boundary_y0(x, on_boundary):
     """
-    Observation function for boundary condition at x = 0.
+    Boundary condition function for x = 0.
     
     Args:
         x (np.ndarray): The coordinates.
-        theta (np.ndarray): The theta values.
-        X (np.ndarray): Additional parameters.
+        on_boundary (bool): Whether the point is on the boundary.
     
     Returns:
-        np.ndarray: Difference between x[:, 1:2] and theta.
+        bool: True if on the boundary and x[0] is close to 0.
     """
-    return x[:, 1:2] - theta
+    return on_boundary and np.isclose(x[1], 0)
+
+def boundary_y1(x, on_boundary):
+    """
+    Boundary condition function for x = 1.
+    
+    Args:
+        x (np.ndarray): The coordinates.
+        on_boundary (bool): Whether the point is on the boundary.
+    
+    Returns:
+        bool: True if on the boundary and x[0] is close to 1.
+    """
+    return on_boundary and np.isclose(x[1], 1)
+
+
+# def bc0_obs(x, theta, X):
+#     """
+#     Observation function for boundary condition at x = 0.
+    
+#     Args:
+#         x (np.ndarray): The coordinates.
+#         theta (np.ndarray): The theta values.
+#         X (np.ndarray): Additional parameters.
+    
+#     Returns:
+#         np.ndarray: Difference between x[:, 1:2] and theta.
+#     """
+#     return x[:, 1:2] - theta
 
 def output_transform(x, y):
     """
@@ -171,29 +143,25 @@ def create_nbho(name, cfg):
     
     a1 = (cfg.data.rhoc * cfg.data.L0**2) / (cfg.data.tauf * cfg.data.k)
     a2 = (cfg.data.rhob * cfg.data.L0**2 * cfg.data.cb * cfg.data.Wb) / (cfg.data.k)
-    a3 = 0 
+    a3 = 0
 
     def pde(x, u):
-        #   x[:,0] = spatial coordinate
-        #   x[:,1] = temporal coordinate
-        #   y = output of the network (Temperature T if Bio-Heat Equation)
         
-        du_t = dde.grad.jacobian(u, x, i=0, j=3)
+        du_t = dde.grad.jacobian(u, x, i=0, j=2)
         du_xx = dde.grad.hessian(u, x, i=0, j=0)
-        du_yy = dde.grad.hessian(u, x, i=1, j=1)
-        
-        
-        u_t = dde.grad.jacobian(u, x, i=0, j=0)
-        u_xx = dde.grad.hessian(u, x, i=1, j=1)
-        u_yy = dde.grad.hessian(u, x, i=2, j=2)
-        points1=Table[{x, t, usol[x,,t]},{t,0,1,0.05},{x,0,1, 0.1}];
-        return (a1 * dy_t - dy_xx + a2*y) 
+        du_yy = dde.grad.hessian(u, x, i=0, j=1)
+
+        return (a1 * du_t - (du_xx + du_yy) + a2*u - a3) 
         
     def bc1_obs(x, theta, X):
         dtheta_x = dde.grad.jacobian(theta, x, i=0, j=0)
         # return dtheta_x - (h/k)*(x[:, 3:4]-x[:, 2:3]) - K * (x[:, 2:3] - theta)
         return dtheta_x - (cfg.data.h / cfg.data.k)*(x[:, 2:3]-x[:, 1:2]) - K * (x[:, 1:2] - theta)
-
+    
+    def bc2_obs(x, theta, X):
+        dtheta_x = dde.grad.jacobian(theta, x, i=0, j=0)
+        # return dtheta_x - (h/k)*(x[:, 3:4]-x[:, 2:3]) - K * (x[:, 2:3] - theta)
+        return dtheta_x - (cfg.data.h / cfg.data.k)*(x[:, 2:3]-x[:, 1:2]) - K * (x[:, 1:2] - theta)
 
     def ic_obs(x):
 
@@ -219,23 +187,21 @@ def create_nbho(name, cfg):
     timedomain = dde.geometry.TimeDomain(0, 1)
     geomtime = dde.geometry.GeometryXTime(geom, timedomain)
 
-    # Initial Conditions.
-    ic = dde.icbc.IC(geomtime, ic_obs, lambda _, on_initial: on_initial)
+    # bc_0 = dde.icbc.OperatorBC(geomtime, bc0_obs, boundary_0)
     
-    # Bounday Conditions.
-    bc_1 = dde.icbc.OperatorBC(geomtime, bc1_obs, boundary_1)
     
-    # Dirichlet boundary condition for theta(0, tau) = c0
-    bc_1 = dde.icbc.DirichletBC(geomtime, lambda x: c0, boundary_1, component=0)
+    bcx_0 = dde.icbc.NeumannBC(geomtime, 0, boundary_x0, component=0)
+    bcx_1 = dde.icbc.NeumannBC(geomtime, 0, boundary_x1, component=0)
+    
+    bcy_0 = dde.icbc.DirichletBC(geomtime, lambda x: 0, boundary_y0, component=0)
+    # bcy_1 =  ### TO DO
 
-    # Neumann boundary condition for partial_X theta(1, tau) = v(tau)
-    bc_2 = dde.icbc.NeumannBC(geomtime, v_tau, boundary_right, component=0)
+    ic = dde.icbc.IC(geomtime, ic_obs, lambda _, on_initial: on_initial)
 
     data = dde.data.TimePDE(
         geomtime,
         pde,
-        # [bc_0, bc_1, ic],
-        [bc_1, ic],
+        [bcx_1, ic],
         num_domain=2560,
         num_boundary=200,
         num_initial=100,
