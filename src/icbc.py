4b1a4e28e0955e8a55e5bcf454d483e18fcd57ce
EugenioBugli <eugeniobugli15@gmail.com>
2024-08-14 13:33:02 +0200

    boundary conditions

--- src/icbc.py
+++ src/icbc.py
@@ -7,11 +7,11 @@ Inside this file you will find all the definitions of the boundary and initial c
 Since we are dealing with a 2D problem, 
 we will have 4 boundary conditions for our surface, which is modeled as a square with sides of length 1:
 
-    |> X = 0 left  side
-    |> X = 1 right side
+    |> X = 0 left  side (Dirichlet)
+    |> X = 1 right side (Neumann)
 
-    |> Y = 0 lower side
-    |> Y = 1 upper side
+    |> Y = 0 lower side (Neumann)
+    |> Y = 1 upper side (Neumann)
 
 
  [Y]
@@ -80,9 +80,82 @@ left_bc = dde.icbc.DirichletBC(
 
 # Boundary condition for the right part of the square (X=1): dU(1,y,t)/dX = t
 
+def right_boundary(x, on_boundary):
+    """
+    This function identifies the points where the boundary condition should be applied.
+
+    Args:
+        x : our input, which is a 3D vector with a 2D space domain and 1D time domain
+            x : x coordinate    (x[:,0])
+            y : y coordinate    (x[:,1])
+            t : time coordinate (x[:,2])
+
+        on_boundary : boolean that specifies whether the point x is on the boundary of the domain.
+        
+    Return:
+        boolean value
+    """
+
+    # here we use x[0] since this function is applied to one point at a time
+    return on_boundary and np.isclose(x[0], 1)
+
+right_bc = dde.icbc.NeumannBC(
+    geomtime,
+    lambda x: x[2],
+    right_boundary 
+)
 
 # Boundary condition for the lower part of the square (Y=0): dU(x,0,t)/dX = 0
 
+def lower_boundary(x, on_boundary):
+    """
+    This function identifies the points where the boundary condition should be applied.
+
+    Args:
+        x : our input, which is a 3D vector with a 2D space domain and 1D time domain
+            x : x coordinate    (x[:,0])
+            y : y coordinate    (x[:,1])
+            t : time coordinate (x[:,2])
+
+        on_boundary : boolean that specifies whether the point x is on the boundary of the domain.
+        
+    Return:
+        boolean value
+    """
+
+    # here we use x[1] since this function is applied to one point at a time
+    return on_boundary and np.isclose(x[1], 0)
+
+lower_bc = dde.icbc.NeumannBC(
+    geomtime,
+    lambda x: 0,
+    lower_boundary 
+)
 
 
-# Boundary condition for the upper part of the square (Y=1): dU(x,1,t)/dX = t
+# Boundary condition for the upper part of the square (Y=1): dU(x,1,t)/dX = 0
+
+def upper_boundary(x, on_boundary):
+    """
+    This function identifies the points where the boundary condition should be applied.
+
+    Args:
+        x : our input, which is a 3D vector with a 2D space domain and 1D time domain
+            x : x coordinate    (x[:,0])
+            y : y coordinate    (x[:,1])
+            t : time coordinate (x[:,2])
+
+        on_boundary : boolean that specifies whether the point x is on the boundary of the domain.
+        
+    Return:
+        boolean value
+    """
+
+    # here we use x[1] since this function is applied to one point at a time
+    return on_boundary and np.isclose(x[1], 1)
+
+upper_bc = dde.icbc.NeumannBC(
+    geomtime,
+    lambda x: 0,
+    upper_boundary 
+)
