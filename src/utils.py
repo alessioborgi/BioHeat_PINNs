d0907616027b736bf58b938c0e7e2ec02eaa2aa1
EugenioBugli <eugeniobugli15@gmail.com>
2024-08-23 15:52:10 +0200

    utils

--- src/utils.py
+++ src/utils.py
@@ -10,22 +10,55 @@ import numpy as np
 import torch
 import os
 import json
-import main
+import torch
+import torch.nn.functional as F
 
+# Setting the device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-
-# Initialize global variables
-# L0 = tauf = k = p0 = d = rhoc = cb = h = Tmin = Tmax = alpha = W = steep = tchange = None
-properties = {
-    "L0": None, "tauf": None, "k": None, "p0": None, "d": None,
-    "rhoc": None, "cb": None, "h": None, "Tmin": None, "Tmax": None, "alpha": None,
-    "W": None, "steep": None, "tchange": None, "rhob": None, "Wb": None
-}
-
+run = ""
+figures_dir = "./tests/figures"
+current_file = os.path.abspath(__file__)
+src_dir = os.path.dirname(current_file)
+project_dir = os.path.dirname(src_dir)
+tests_dir = os.path.join(project_dir, "tests")
 f1, f2, f3 = [None]*3
 
+def open_json_config(run_type):
+    """
+    Open the .json configuration file
+    
+    Args:
+        run_type (str): This specifies the group of parameters 
+    
+    Returns:
+        Dictionary that contains all the parameters
+    """
+    # Construct the relative path
+    path = './mathematica/TwoDim/' + run_type + '/data_2D_0.json'
+    
+    # Convert to absolute path
+    abs_path = os.path.abspath(path)
+    
+    # Print current working directory and the absolute path for debugging
+    #print("Current Working Directory:", os.getcwd())
+    #print("Absolute Path:", abs_path)
 
+    # Check if the file exists
+    if not os.path.exists(abs_path):
+        print(f"File does not exist: {abs_path}")
+        return None  # or raise an exception
+
+    # If file exists, proceed to open and load it
+    try:
+        with open(abs_path, 'r') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        print(f"File not found: {abs_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        
 def seed_all(seed):
     """
     Sets the seed for all random number generators used in the project.
@@ -67,10 +100,10 @@ def set_name(prj, run):
     name = f"{run}"
 
 
-    general_model = os.path.join(main.tests_dir, "models")
+    general_model = os.path.join(tests_dir, "models")
     os.makedirs(general_model, exist_ok=True)
 
-    general_figures = os.path.join(main.tests_dir, "figures")
+    general_figures = os.path.join(tests_dir, "figures")
     # os.makedirs(general_figures, exist_ok=True)
 
     model_dir = os.path.join(general_model, name)
@@ -80,40 +113,6 @@ def set_name(prj, run):
     os.makedirs(figures_dir, exist_ok=True)
 
     return name, general_figures, model_dir, figures_dir
-
-def get_properties(n):
-    """
-    Retrieves and returns properties related to the model or dataset.
-    
-    This function can include information such as data shapes, model parameters,
-    or other relevant metadata required for training or evaluation.
-    
-    Args:
-        None
-    
-    Returns:
-        None
-    """
-    global L0, tauf, k, p0, d, rhoc, cb, h, Tmin, Tmax, alpha, W, steep, tchange
-    file_path = os.path.join(main.src_dir, 'data_simulations', f'data{n}.json')
-
-    # Open the file and load the JSON data
-    with open(file_path, 'r') as f:
-        data = json.load(f)
-
-    properties.update(data['Parameters'])
-    par = data['Parameters']
-    local_vars = locals()
-    for key in par:
-        if key in local_vars:
-            local_vars[key] = par[key]
-
-    L0, tauf, k, p0, d, rhoc, cb, h, Tmin, Tmax, alpha, W, steep, tchange = (
-        par["L0"], par["tauf"], par["k"], par["p0"], par["d"], par["rhoc"],
-        par["cb"], par["h"], par["Tmin"], par["Tmax"], par["alpha"], par["W"], par["steep"], par["tchange"]
-    )
-
-f1, f2, f3 = [None]*3
 
 def mish(x):
     return x * torch.tanh(F.softplus(x))
@@ -135,4 +134,4 @@ def output_transform(x, y):
     Returns:
         np.ndarray: Transformed output.
     """
-    return x[:, 0:1] * y
+    return x[:, 0:1] * y  
